f8248a231d19a4d335bf76526dfd81076cb7b5c3
kpilszak <klaudia.pilszak@gmail.com>
2024-07-23 22:24:06 -0400

    Fetch all videos

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,43 @@
+import { gql, GraphQLClient } from 'graphql-request'
+
+export const getStaticProps = async () => {
+  const url = process.env.ENDPOINT
+  const graphqlClient = new GraphQLClient(url, {
+    headers: {
+      "Authorization": process.env.GRAPH_CMS_TOKEN
+    }
+  })
+
+  const query = gql`
+    query {
+      videos {
+        createdAt,
+        id,
+        title,
+        description,
+        seen,
+        slug,
+        tags,
+        thumbnail {
+          url
+        },
+        mp4 {
+          url
+        }
+      }
+    }`
+
+  const data = await graphqlClient.request(query)
+  const videos = data.videos
+  console.log(data)
+
+  return {
+    props: {
+      videos,
+    }
+  }
+}
+
 const Home = () => {
   return (
     <div>
